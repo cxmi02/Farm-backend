2a3aa0923f78740e5dc757b14af102795517fef4
cxmi02 <149894510+cxmi02@users.noreply.github.com>
2024-09-07 19:02:17 -0500

    Creating the database, configuring AppModule and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import dbConfig from './libs/persistence/db-config'
+import { PersistenceModule } from './libs/persistence';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    PersistenceModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
